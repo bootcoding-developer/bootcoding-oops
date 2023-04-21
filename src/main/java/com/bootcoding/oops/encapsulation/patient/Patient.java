b45716af9f389434f5409dfb9a7b26d84cd47cc8
bootcoding-developer <bootcoding.dev@gmail.com>
2023-04-21 19:58:30 +0530

    Encapsulation examples

---
+++ src/main/java/com/bootcoding/oops/encapsulation/patient/Patient.java
@@ -0,0 +1,18 @@
+package com.bootcoding.oops.encapsulation.patient;
+
+import java.util.Date;
+
+public class Patient {
+
+    private String disease;
+    public String name;
+    public Date dateOfAdmission;
+    public Date dischargeDate;
+
+    public void print(){
+        System.out.println("Name: "+ name);
+        System.out.println("Date of Admission: "+ dateOfAdmission);
+        System.out.println("Date of Discharge: "+ dischargeDate);
+        System.out.println("Disease: "+ disease);
+    }
+}
---
+++ src/main/java/com/bootcoding/oops/encapsulation/patient/PatientHistory.java
@@ -0,0 +1,8 @@
+package com.bootcoding.oops.encapsulation.patient;
+
+public class PatientHistory {
+
+    private String patientId;
+    private int count;
+
+}
